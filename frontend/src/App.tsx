732a2185e0616ed6b0d24498d9700edcb934bed3
Ahmed Lekan <diplomaticfatman66@gmail.com>
2024-04-15 04:33:56 -0700

    frontend register setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
-
 import Layout from "./layouts/Layout"
+import Register from "./pages/Register"
+
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><span>Home page</span></Layout>} />
         <Route path="/search" element={<Layout><span>Search page</span></Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
+
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
