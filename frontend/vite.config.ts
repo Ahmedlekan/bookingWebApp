a9e7ebdfa0b80d1742bf37361cd9376b17dfc80a
Ahmed Lekan <diplomaticfatman66@gmail.com>
2024-10-23 09:22:44 -0700

    vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './src', // Adjust this if needed (by default, root is project directory)
+  build: {
+    outDir: 'dist', // The output directory
+    rollupOptions: {
+      input: './index.html' // Ensure this is correct
+    }
+  }
 })
