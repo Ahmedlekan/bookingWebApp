a7812da6fbea36f00285d830b498145cb9411611
Ahmed Lekan <diplomaticfatman66@gmail.com>
2024-10-24 07:31:34 -0700

    vite modified

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api' : {
-        target: "http://localhost:7000/",
-        changeOrigin: true
-      }
+  base: './', // This ensures relative paths for production
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure this path points correctly to the root
     }
   }
 })
